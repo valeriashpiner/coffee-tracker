e9b1aef893ee3796b0992bf93da5420e5f26d622
Valeria Shpiner <shpiner.v@avocadotechnology.com>
2018-04-16 15:44:31 +0300

    Integrate google-maps

--- ios/CoffeeTracker/AppDelegate.m
+++ ios/CoffeeTracker/AppDelegate.m
@@ -9,11 +9,14 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+@import GoogleMaps; //add this line if you want to use Google Maps
+
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyCL3Q6VxbTebp-GYaYs1nA35shGHomFQdY"]; // add this line using the api key obtained from Google Console
   NSURL *jsCodeLocation;
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
--- src/containers/search/index.js
+++ src/containers/search/index.js
@@ -1,12 +1,40 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default class SearchScreen extends React.Component {
     render() {
       return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-          <Text>Search</Text>
+        <View style={styles.container}>
+          <MapView
+          style={styles.map}
+    initialRegion={{
+      latitude: 37.78825,
+      longitude: -122.4324,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    }}
+  />
         </View>
       );
     }
   }
+
+  const styles = StyleSheet.create({
+    container: {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      justifyContent: 'flex-end',
+      alignItems: 'center',
+    },
+    map: {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+    },
+  });
